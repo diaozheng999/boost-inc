27f9deb8218080f1cc0341b995907c6189aa956b
Diao Zheng <diaozheng999@gmail.com>
2021-02-12 14:51:42 +0800

    rewrite map

--- src/Inc.ml
+++ src/Inc.ml
@@ -7,44 +7,36 @@ let lift (b: 'b Var.t) ~f =
   let lifted av = lift (Box.indexOf av) av f in
   fun () -> b.modref >>= lifted
 
-let map (a: 'a Var.t) ~f =
+let mkMap (a: 'a Var.t) f =
   let f aref =
-      aref >>= fun av ->
-        let a = Box.valueOf av in
-        write (Box.create (f a))
-      in
-  let lifted = lift ~f a in
-  Var.ofCombinator (lifted ())
+    aref >>= fun av ->
+      let a = Box.valueOf av in
+      write (Box.create (f a))
+    in
+  lift ~f a ()
+
+let mkMap2 a b f =
+  let mapped1 av = 
+    let a = Box.valueOf av in
+    mkMap b (f a) in
+  lift a ~f:(fun aref -> aref >>= mapped1) ()
+
+let mkMap3 a b c f =
+  let mapped1 av =
+    let a = Box.valueOf av in
+    mkMap2 b c (f a) in
+  lift a ~f:(fun aref -> aref >>= mapped1) ()
+
+let mkMap4 a b c d f =
+  let mapped1 av =
+    let a = Box.valueOf av in
+    mkMap3 b c d (f a) in
+  lift a ~f:(fun aref -> aref >>= mapped1) ()
+
+let map a ~f = Var.ofCombinator (mkMap a f)
   
-let map2 (a: 'a Var.t) (b: 'b Var.t) ~f =
-  let mapped2 av bv =
-    let a = Box.valueOf av in
-    let b = Box.valueOf bv in
-    write (Box.create (f a b))
-  in
-  let mapped1 av = b.modref >>= mapped2 av in
-  Var.ofCombinator (a.modref >>= mapped1)
+let map2 a b ~f = Var.ofCombinator (mkMap2 a b f)
 
-let map3 (a: 'a Var.t) (b: 'b Var.t) (c: 'c Var.t) ~f =
-  let mapped3 av bv cv =
-    let a = Box.valueOf av in
-    let b = Box.valueOf bv in
-    let c = Box.valueOf cv in
-    write (Box.create (f a b c))
-  in
-  let mapped2 av bv = c.modref >>= mapped3 av bv in
-  let mapped1 av = b.modref >>= mapped2 av in
-  Var.ofCombinator (a.modref >>= mapped1)
+let map3 a b c ~f = Var.ofCombinator (mkMap3 a b c f)
 
-let map4 (a: 'a Var.t) (b: 'b Var.t) (c: 'c Var.t) (d: 'd Var.t) ~f =
-  let mapped4 av bv cv dv =
-    let a = Box.valueOf av in
-    let b = Box.valueOf bv in
-    let c = Box.valueOf cv in
-    let d = Box.valueOf dv in
-    write (Box.create (f a b c d))
-  in
-  let mapped3 av bv cv = d.modref >>= mapped4 av bv cv in
-  let mapped2 av bv = c.modref >>= mapped3 av bv in
-  let mapped1 av = b.modref >>= mapped2 av in
-  Var.ofCombinator (a.modref >>= mapped1)
+let map4 a b c d ~f = Var.ofCombinator (mkMap4 a b c d f)
